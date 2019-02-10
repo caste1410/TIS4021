0af0e67f708ae27dcd2667509797baaab402ea98
caste1410 <pumas_141096@hotmail.com>
2019-02-09 22:38:47 -0600

    funcion setStart
    Define el inicio donde iniciara el agente

--- Qlearning/GameScene.swift
+++ Qlearning/GameScene.swift
@@ -11,13 +11,34 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    let camino = [[0,0,-1,0,0,1],
+    var path = [[0,0,-1,0,0,1],
                   [-1,0,0,0,0,0],
                   [0,-1,0,0,-1,-1],
                   [0,0,0,0,0,0],
                   [0,0,-1,0,0,-1],
                   [0,0,0,0,-1,0],
-                  [2,0,0,-1,0,0]]
+                  [0,0,0,-1,0,0]]
+    
+    var graphicPath : [[SKSpriteNode]] = []
+    
+    func setStart(point: [Int]) -> [Int]{
+        var startPoint = point
+        var randomRow = Int.random(in: 0..<6)
+        var randomColumn = Int.random(in: 0..<7)
+        
+        if path[point[0]][point[1]] == 0{
+            path[point[0]][point[1]] = 2
+        }else{
+            while path[randomRow][randomColumn] != 0{
+                randomRow = Int.random(in: 0..<6)
+                randomColumn = Int.random(in: 0..<7)
+            }
+            path[randomRow][randomColumn] = 2
+            startPoint = [randomRow,randomColumn]
+        }
+        return startPoint
+    }
+    
     
 
 }
